38c7bd52f1cd7252963d545b8400598820fe3dd3
Arnab Ghosh <arnabghs@thoughtworks.com>
2023-07-11 16:35:45 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useLayoutEffect, useRef} from 'react';
+import './App.scss';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="panels" id="panelContainer">
+            <div className="panel one" id="panel_1">
+                <div className="clip">
+                    <div className="video_clip">
+                        <iframe
+                            src="https://player.vimeo.com/video/468155263?&api=1&controls=0&background=1&autoplay=1&loop=1&mute=1"
+                            allow="autoplay;"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div className="panel two" id="panel_2">
+                <div className="clip">
+                    <img src="https://source.unsplash.com/800x600?ocean" alt=""/>
+                </div>
+            </div>
+
+            <div className="panel three" id="panel_3">
+                <div className="clip">
+                    <img src="https://source.unsplash.com/800x600?river" alt=""/>
+                </div>
+            </div>
+
+            <div className="panel four" id="panel_4">
+                <div className="clip">
+                    <img src="https://source.unsplash.com/800x600?waterfall" alt=""/>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
